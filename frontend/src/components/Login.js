5efba71c94f093b5a267f3143fc73aa41255efc6
sathwikboddu <boddusathwik45@gmail.com>
2025-02-09 20:35:23 +0530

    append

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { API_BASE_URL, setAuthToken } from "../config";
 import "./Login.css"; // Import the CSS file
 
+console.log("API BASE URL:", process.env.REACT_APP_API_BASE_URL);
+
 const Login = ({ setUser }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
