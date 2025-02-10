e220f8ce4b44cbd28166933faa9f642162d4f4a3
sathwikboddu <boddusathwik45@gmail.com>
2025-02-10 21:04:45 +0530

    NaN fix

---
+++ search_app/migrations/0010_update_rating_nan.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+def update_rating(apps, schema_editor):
+    MyModel = apps.get_model('search_app', 'MyModel')  # Replace 'MyModel' with your actual model name
+    MyModel.objects.filter(rating__isnull=True).update(rating=3)  # Update NaN (NULL) values to 3
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('search_app', '0009_create_superuser'),  # Update with the last migration filename
+    ]
+
+    operations = [
+        migrations.RunPython(update_rating),
+    ]
